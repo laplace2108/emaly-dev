7af20eee2d4b209791dca662ae476c6ae7d2c5e7
Yosdel Plasencia Montesino <laplace2108@gmail.com>
2019-05-05 13:29:20 -0500

    Comit passport configuration

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-const express = require("express");
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ bye: "buddy" });
-});
+//console.developers.google.com
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecrets,
+      callBackURL: '/auth/google/callback'
+    }, 
+    (accessToken) => 
+      {
+        console.log(accessToken);
+      }
+  )
+);
+
+app.get(
+  '/auth/google', 
+  passport.authenticate(
+    'google', 
+    {
+      scope: ['profile', 'email']
+    }
+  )
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
