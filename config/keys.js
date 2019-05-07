9f8fdb5a8abd1d93ccff7055e61b254c967ce733
Yosdel Plasencia Montesino <laplace2108@gmail.com>
2019-05-06 21:12:19 -0500

    finished auth flow

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleClientID: '760458019387-6eunqmfn33dj77d63j84dlg6g7iqtjd5.apps.googleusercontent.com',
+    googleClientSecret: 'LZF8DgonVe6eiX6QdedLHZXQ',
+    mongoURI: 'mongodb+srv://yplasencia:Yosdel21083d1.@testecus-fabas.mongodb.net/test?retryWrites=true',
+    cookieKey: 'wnwfjoerfpoermfpoerkmpfokermpfokerp'
+};
+
